9a2802d3501ef0079665f7211e9ef99ca4941a05
maariasGH <ariasmariano100@gmial.com>
2026-03-03 21:27:09 -0300

    Generación de la BD

--- bdrdam-port.sql
+++ bdrdam-port.sql
@@ -2,38 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict 8SN6YViwgzygcS0sg3uK3bvVbLhMq142MF9ZNv8amsH6xE7DxSVjNx1EQeSXFML
+\restrict RmWi3HNJW3oqVrMBoKLQpco8NrjwXKWbTIu8JKnifDnaIO2oX1hKSZQrcNWBLaP
 
 -- Dumped from database version 18.2
 -- Dumped by pg_dump version 18.2
 
--- Started on 2026-02-25 18:11:36
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- TOC entry 5072 (class 1262 OID 16388)
--- Name: rdam_db; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE rdam_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'es-AR';
-
-
-ALTER DATABASE rdam_db OWNER TO postgres;
-
-\unrestrict 8SN6YViwgzygcS0sg3uK3bvVbLhMq142MF9ZNv8amsH6xE7DxSVjNx1EQeSXFML
-\connect rdam_db
-\restrict 8SN6YViwgzygcS0sg3uK3bvVbLhMq142MF9ZNv8amsH6xE7DxSVjNx1EQeSXFML
+-- Started on 2026-03-03 21:25:58
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -54,11 +28,11 @@ SET row_security = off;
 
 CREATE TYPE public.estado_tramite AS ENUM (
     'PENDIENTE',
-    'VENCIDO',
-    'PAGADO',
-    'EMITIDO',
-    'RECHAZADO',
-    'EMITIDO_VENCIDO'
+    'VENCIDA',
+    'PAGADA',
+    'EMITIDA',
+    'RECHAZADA',
+    'EMITIDA_VENCIDA'
 );
 
 
@@ -122,7 +96,8 @@ CREATE TABLE public.certificados (
     certificado_id integer NOT NULL,
     tramite_id integer,
     fecha_emision timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    url_archivo_s3 text
+    url_archivo_s3 text,
+    usuario_id integer
 );
 
 
@@ -249,7 +224,7 @@ CREATE TABLE public.usuarios (
     usuario_id integer NOT NULL,
     nombre_completo character varying(100) NOT NULL,
     login character varying(50) NOT NULL,
-    password_hash text NOT NULL,
+    password_hash text,
     rol public.rol_usuario NOT NULL,
     estado character varying(20) DEFAULT 'Activo'::character varying,
     fecha_creacion timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
@@ -317,45 +292,53 @@ ALTER TABLE ONLY public.usuarios ALTER COLUMN usuario_id SET DEFAULT nextval('pu
 
 
 --
--- TOC entry 5066 (class 0 OID 16465)
+-- TOC entry 5067 (class 0 OID 16465)
 -- Dependencies: 226
 -- Data for Name: certificados; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.certificados (certificado_id, tramite_id, fecha_emision, url_archivo_s3) FROM stdin;
+COPY public.certificados (certificado_id, tramite_id, fecha_emision, url_archivo_s3, usuario_id) FROM stdin;
+1	3	2026-03-03 19:02:11.689331	aa	3
 \.
 
 
 --
--- TOC entry 5064 (class 0 OID 16450)
+-- TOC entry 5065 (class 0 OID 16450)
 -- Dependencies: 224
 -- Data for Name: pagos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.pagos (pago_id, tramite_id, external_reference, monto, fecha_pago, pagado) FROM stdin;
+1	1	\N	1250.00	\N	f
+2	3	\N	1250.00	\N	f
+3	4	\N	1250.00	\N	f
 \.
 
 
 --
--- TOC entry 5062 (class 0 OID 16430)
+-- TOC entry 5063 (class 0 OID 16430)
 -- Dependencies: 222
 -- Data for Name: tramites; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.tramites (tramite_id, cuil, nombre_solicitante, email_contacto, estado, fecha_solicitud, usuario_creador_id, fecha_ultima_modificacion, ciudad_solicitante) FROM stdin;
 1	20334445556	Juan Perez	juan@example.com	PENDIENTE	2026-02-25	2	2026-02-25 17:41:37.510025	Santa Fe
+4	111111111111	axel	axel@example.com	EMITIDA	2026-03-03	1	2026-03-03 18:56:01.761545	Arabia
+3	20461340602	Mariano Arias	marian@example.com	PAGADA	2026-03-03	1	2026-03-03 19:02:11.689331	Santa Fe
 \.
 
 
 --
--- TOC entry 5060 (class 0 OID 16412)
+-- TOC entry 5061 (class 0 OID 16412)
 -- Dependencies: 220
 -- Data for Name: usuarios; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 COPY public.usuarios (usuario_id, nombre_completo, login, password_hash, rol, estado, fecha_creacion, registrador_id) FROM stdin;
-1	Admin Sistema	admin	pbkdf2:sha256:250000$xxxx	ADMINISTRADOR	Activo	2026-02-25 17:41:37.504766	\N
-2	Juan Perez	jperez	pbkdf2:sha256:250000$yyyy	CIUDADANO	Activo	2026-02-25 17:41:37.508524	\N
+1	Admin Sistema	admin	pbkdf2:sha256:250000$xxxx	ADMINISTRADOR	Activo	2026-02-25 17:41:37.504766	1
+2	Mariano Arias	mariano	pbkdf2:sha256:250000$yyyy	CIUDADANO	Activo	2026-02-25 17:41:37.508524	1
+3	Carlos	car	123	OPERADOR	Activo	2026-03-03 18:35:36.702615	\N
+4	Ciudadano	ciudadano	null	CIUDADANO	Activo	2026-03-03 18:37:38.822296	\N
 \.
 
 
@@ -365,7 +348,7 @@ COPY public.usuarios (usuario_id, nombre_completo, login, password_hash, rol, es
 -- Name: certificados_certificado_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.certificados_certificado_id_seq', 1, false);
+SELECT pg_catalog.setval('public.certificados_certificado_id_seq', 1, true);
 
 
 --
@@ -374,7 +357,7 @@ SELECT pg_catalog.setval('public.certificados_certificado_id_seq', 1, false);
 -- Name: pagos_pago_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.pagos_pago_id_seq', 1, false);
+SELECT pg_catalog.setval('public.pagos_pago_id_seq', 3, true);
 
 
 --
@@ -383,7 +366,7 @@ SELECT pg_catalog.setval('public.pagos_pago_id_seq', 1, false);
 -- Name: tramites_tramite_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.tramites_tramite_id_seq', 1, true);
+SELECT pg_catalog.setval('public.tramites_tramite_id_seq', 4, true);
 
 
 --
@@ -392,7 +375,7 @@ SELECT pg_catalog.setval('public.tramites_tramite_id_seq', 1, true);
 -- Name: usuarios_usuario_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.usuarios_usuario_id_seq', 2, true);
+SELECT pg_catalog.setval('public.usuarios_usuario_id_seq', 5, true);
 
 
 --
@@ -474,6 +457,15 @@ ALTER TABLE ONLY public.certificados
 
 
 --
+-- TOC entry 4912 (class 2606 OID 16510)
+-- Name: certificados fk_certificado_usuario; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.certificados
+    ADD CONSTRAINT fk_certificado_usuario FOREIGN KEY (usuario_id) REFERENCES public.usuarios(usuario_id);
+
+
+--
 -- TOC entry 4910 (class 2606 OID 16459)
 -- Name: pagos pagos_tramite_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
@@ -500,11 +492,11 @@ ALTER TABLE ONLY public.usuarios
     ADD CONSTRAINT usuarios_registrador_id_fkey FOREIGN KEY (registrador_id) REFERENCES public.usuarios(usuario_id);
 
 
--- Completed on 2026-02-25 18:11:36
+-- Completed on 2026-03-03 21:25:58
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 8SN6YViwgzygcS0sg3uK3bvVbLhMq142MF9ZNv8amsH6xE7DxSVjNx1EQeSXFML
+\unrestrict RmWi3HNJW3oqVrMBoKLQpco8NrjwXKWbTIu8JKnifDnaIO2oX1hKSZQrcNWBLaP
 
