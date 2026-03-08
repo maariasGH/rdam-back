d4e89d5c2c39b8ff15aa9ad8d7c8d3f9d7e90e21
maariasGH <ariasmariano100@gmial.com>
2026-03-08 00:15:57 -0300

    Finalización y Seteo exitoso de funcionamiento del proyecto con Docker

--- app/database.py
+++ app/database.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv 
 
+load_dotenv()  # carga el archivo .env
 # Cambia estos datos por tus credenciales de PostgreSQL o Neon.tech
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:0406@localhost:5432/rdam_db"
-
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
